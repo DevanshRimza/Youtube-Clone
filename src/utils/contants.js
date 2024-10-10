0c5fe68239908cfebea004565d6d38f11201c5aa
DevanshRimza <113292116+DevanshRimza@users.noreply.github.com>
2024-10-10 23:32:33 +0530

    Add files via upload

---
+++ src/utils/contants.js
@@ -0,0 +1,18 @@
+const GOOGLE_API_KEY = "AIzaSyBXH4CDYu_YYuYcNQFRYuoiskPfoG2pHqs";
+
+//const GOOGLE_API_KEY = "AIzaSyAtIibgU4boIyzkgbabBCe8BJJsBSfNlWA";
+// AIzaSyAJJY98gF3pgi_5th4SOnenUjOKvImjnh0
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+  // https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+ //http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=Query
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
